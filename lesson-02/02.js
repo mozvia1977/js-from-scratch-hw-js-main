bfa56d0cbb4b412c51a57aaef14f0ab8f4c4d840
mozvia1977 <mozharsl1977@gmail.com>
2026-02-10 21:04:25 +0300

    second HW

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,10 +7,10 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
+let dice1 = Math.floor((Math.random()*6)+1);// your code
+let dice2 = Math.floor((Math.random()*6)+1);// your code
 let isWinningDouble // your code
-
+if (dice1 > 3 && dice1 === dice2) {isWinningDouble = true} else {isWinningDouble = false};
 /*
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
